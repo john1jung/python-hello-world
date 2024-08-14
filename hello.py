79794bc1c2b29c1696731b218b44b4e743ff4399
John Jung <jjung@c2n.local>
2024-08-13 20:25:32 -0500

    testing!

--- hello.py
+++ hello.py
@@ -4,9 +4,8 @@
 # to the console (the panel on the right)
 
 print('"Hello, World!"'), 
-print("Goodbye, Now.")
 # Modify your code on line 7 to try to accomplish the tasks listed below:
-
+print("Goodbye, Now.")
 #   1. Change the message that is printed.
 #   2. Figure out what the parentheses do. Will the code work without them?
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
@@ -16,3 +15,6 @@ print("Goodbye, Now.")
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
 #   8. Other. You choose!
 
+name = input("Where do you live? ")
+print("Hello " + name);
+print(name + " is a great place to live!")
